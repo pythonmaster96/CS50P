1a27edb26b6054e9a09d6be3e6cbb8997d93b5bf
pythonmaster96 <djokicdejan.ekof@gmail.com>
2026-05-26 22:22:08 +0200

    Add files via upload
    Uploaded challenges in Lecture0

---
+++ Lecture _0/challenge_04_full_name.py
@@ -0,0 +1,6 @@
+# Challenge 04 - Full Name Builder
+# Goal: Ask for first and last name, print in Last, First format
+
+first = input("What is your first name? ")
+last = input("What is your last name? ")
+print(f"{last}, {first}")
