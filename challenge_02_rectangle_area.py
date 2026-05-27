c0bf229d63bfdbf3b3497dc95ded00fa722330c7
pythonmaster96 <djokicdejan.ekof@gmail.com>
2026-05-27 13:34:30 +0200

    Delete challenge_02_rectangle_area.py

--- challenge_02_rectangle_area.py
+++
@@ -1,7 +0,0 @@
-# Challenge 02 - Rectangle Area
-# Goal: Calculate the area of a rectangle
-
-width = int(input("What is width of rectangle? "))
-height = int(input("What is height of rectangle? "))
-area = width * height
-print(f"Area: {area}")
