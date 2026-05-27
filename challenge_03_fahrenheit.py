460f1fe1c6a7df0a1e559ba975a260068558da7e
pythonmaster96 <djokicdejan.ekof@gmail.com>
2026-05-27 13:34:49 +0200

    Delete challenge_03_fahrenheit.py

--- challenge_03_fahrenheit.py
+++
@@ -1,6 +0,0 @@
-# Challenge 03 - Fahrenheit to Celsius Converter
-# Goal: Convert a temperature from Fahrenheit to Celsius
-
-fahrenheit = float(input("What is the temperature in Fahrenheit? "))
-calculated_celsius = (fahrenheit - 32) * 5 / 9
-print(f"{calculated_celsius:.2f}°C")
