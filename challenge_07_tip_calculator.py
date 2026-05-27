cfeeca9d956a9716b13daaa17c46b45d7363ce9f
pythonmaster96 <djokicdejan.ekof@gmail.com>
2026-05-27 13:32:10 +0200

    Delete challenge_07_tip_calculator.py

--- challenge_07_tip_calculator.py
+++
@@ -1,9 +0,0 @@
-# Challenge 07 - Tip Calculator
-# Goal: Calculate tip amount and total bill
-
-bill = float(input("How much is the bill? "))
-tip_percent = int(input("How much is the tip? "))
-tip = tip_percent / 100 * bill
-total = bill + tip
-print(f"Tip: ${tip:.2f}")
-print(f"Total: ${total:.2f}")
