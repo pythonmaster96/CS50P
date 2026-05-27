374f649a030ffaf946be10f49b08fc3628ab900f
pythonmaster96 <djokicdejan.ekof@gmail.com>
2026-05-27 13:35:13 +0200

    Delete challenge_05_bill_splitter.py

--- challenge_05_bill_splitter.py
+++
@@ -1,6 +0,0 @@
-# Challenge 05 - Bill Splitter
-# Goal: Split a restaurant bill equally among friends
-
-bill = float(input("What is the amount of bill? "))
-people = int(input("What is the number of people? "))
-print(f"Each person pays: ${bill / people:.2f}")
