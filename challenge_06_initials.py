21af13a3e8ef6bfbaf5879f1f54ffa128c4501d9
pythonmaster96 <djokicdejan.ekof@gmail.com>
2026-05-27 13:32:52 +0200

    Delete challenge_06_initials.py

--- challenge_06_initials.py
+++
@@ -1,7 +0,0 @@
-# Challenge 06 - Initials Generator
-# Goal: Print initials from first, middle, and last name
-
-first = input("What is your first name? ")
-middle = input("What is your middle name? ")
-last = input("What is your last name? ")
-print(f"{first[0].upper()}.{middle[0].upper()}.{last[0].upper()}.")
