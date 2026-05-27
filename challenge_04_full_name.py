d3604a99372aa02bf9c813c9f4980ccf74d4d1ec
pythonmaster96 <djokicdejan.ekof@gmail.com>
2026-05-27 13:34:59 +0200

    Delete challenge_04_full_name.py

--- challenge_04_full_name.py
+++
@@ -1,6 +0,0 @@
-# Challenge 04 - Full Name Builder
-# Goal: Ask for first and last name, print in Last, First format
-
-first = input("What is your first name? ")
-last = input("What is your last name? ")
-print(f"{last}, {first}")
